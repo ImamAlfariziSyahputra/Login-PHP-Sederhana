8d431cce6654525bf0a32c12610d49d9a08fdce9
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-06-22 11:14:27 +0700

    Menambahkan eskternal link CSS

--- loginphp/login.php
+++ loginphp/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Form Login</title>
 </head>
 <body>
@@ -11,17 +12,17 @@
             <h1>Login</h1>
             <!-- username -->
             <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Masukan username" required="" autofocus="">
+            <input type="text" name="username" placeholder="Masukan username" required="" autofocus="">
             <!-- pass -->
             <label for="password">Password</label>
-            <input type="text" name="password" id="password" placeholder="Masukan password" required="">
+            <input type="password" name="password" placeholder="Masukan password" required="">
             <button type="submit">SUBMIT</button>
         </form>
     </div>
 
     <?php if(isset($_GET['pesan'])) { ?>
     <div class="error">
-        <label for="">Oopss... <?php echo $_GET['pesan']; ?> </label>
+        <label>Oopss... <?php echo $_GET['pesan']; ?> </label>
     </div>
     <?php } ?>
     
